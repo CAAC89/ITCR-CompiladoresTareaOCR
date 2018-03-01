ae6e90d203acdd4a2a93864bb81dcc538d062ac9
WilliamEspinoza <willy3395@gmail.com>
2018-03-01 11:27:40 -0600

    Actualizado para conservar las horas en una lista. Cada hora se separa con split()

--- test.py
+++ test.py
@@ -10,10 +10,11 @@ texto_imagen=pytesseract.image_to_string(imagen)
 with open("salida_horarios.txt",mode ="w")as file:
     file.write(texto_imagen)
 archivo=open("salida_horarios.txt",mode="r")
-patron_horas=re.compile("([01]?[0-9]|2[0-3])(:)[0-5][0-9]")
+patron_horas=re.compile("([01]?[0-9]|2[0-3])(:)([0-5][0-9])")
 horario=[]
-fila_horas=[]
 for linea in archivo:
+    fila_horas=[]
     if patron_horas.match(linea):
-        fila_horas+=[(linea.rstrip()).split()]
-print (fila_horas)
+        fila_horas+=(linea.rstrip()).split()
+        horario+=[fila_horas]
+print (horario)
