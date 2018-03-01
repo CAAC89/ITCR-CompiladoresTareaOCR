fb07c5da2e5df19fb45f76d2fd8f0d75e3cd0240
WilliamEspinoza <willy3395@gmail.com>
2018-03-01 10:30:10 -0600

    Actualizado para obtener horas

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pytesseract
 import os
+import re
 from PIL import Image
 
 directorio= os.path.join(os.path.expanduser("~"),"Downloads","7001-ALA-HER.png")
@@ -8,3 +9,11 @@ texto_imagen=pytesseract.image_to_string(imagen)
 
 with open("salida_horarios.txt",mode ="w")as file:
     file.write(texto_imagen)
+archivo=open("salida_horarios.txt",mode="r")
+patron_horas=re.compile("([01]?[0-9]|2[0-3])(:)[0-5][0-9]")
+horario=[]
+fila_horas=[]
+for linea in archivo:
+    if patron_horas.match(linea):
+        fila_horas+=[(linea.rstrip()).split()]
+print (fila_horas)
