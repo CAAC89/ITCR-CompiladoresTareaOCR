236714251527b7f1c557a1cb2e559b177f132945
WilliamEspinoza <willy3395@gmail.com>
2018-03-01 11:52:01 -0600

    Se agrega metodo para extraer horas con iterador. Se agrega en ambos archivos la escritura de la salida final en un archivo txt

--- ObtenerHorasIter.py
+++ ObtenerHorasIter.py
@@ -10,12 +10,15 @@ texto_imagen=pytesseract.image_to_string(imagen)
 with open("salida_horarios.txt",mode ="w")as file:
     file.write(texto_imagen)
 archivo=open("salida_horarios.txt",mode="r")
-patron_horas=re.compile("([01][0-9][:]|2[0-3][:])([0-5][0-9])")
+patron_horas=re.compile("(?P<horas>[01][0-9][:]|2[0-3][:])(?P<minutos>[0-5][0-9])")
 horario=[]
 for linea in archivo:
     fila_horas=[]
-    if patron_horas.match(linea):
-        fila_horas+=(linea.rstrip()).split()
+    for match in patron_horas.finditer(linea):
+        horas=match.groupdict()["horas"]
+        minutos=match.groupdict()["minutos"]
+        hora=[horas+minutos]
+        fila_horas+=hora
         horario+=[fila_horas]
 with open("salida_horarios.txt",mode ="w")as file:
     for fila in horario:
